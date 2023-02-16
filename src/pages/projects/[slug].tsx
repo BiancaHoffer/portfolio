a35ca935ec87e1eae105212a737ce5d067cdf1eb
Bianca Hoffer <bianca.mhm1@gmail.com>
2023-02-16 19:05:01 +0000

    ajuste tipagem slug

--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -6,86 +6,91 @@ import { ContainerProject } from "./styles";
 import Link from "next/link";
 import { TechnologiesPopover } from "@/components/TechnologiesPopover";
 
-interface Project {
-    uid: string;
-    name: string;
-    image: string;
-    text: string;
-    redirect: {
-        link: string;
-        icon: string;
-    }[];
-    technology: {
-        icon: {
-            url: string;
-            field: string;
-        }
-    }[];
+export interface PropsPost {
+  uid: string;
+  name: string;
+  image: string;
+  text: string;
+  redirect: {
+    link: string;
+    icon: string;
+  }[];
+  technology: {
+    icon: {
+      url: string;
+      field: string;
+    }
+  }[];
+}
+
+export interface Params {
+  params: {
+    slug: string;
+  }
 }
 
 interface ProjectProps {
-    data: Project
+  data: PropsPost
 }
 
 export default function Project({ data }: ProjectProps) {
-    return (
-        <ContainerProject>
-            <h1>{data.name}</h1>
+  return (
+    <ContainerProject>
+      <h1>{data.name}</h1>
 
-            {data.redirect.map(red => {
-                return (
-                    <Link href={red.link}>
-                        <img src={red.icon} />
-                    </Link>
-                )
-            })}
+      {data.redirect.map(red => {
+        return (
+          <Link href={red.link}>
+            <img src={red.icon} />
+          </Link>
+        )
+      })}
 
-            <p>{data.text}</p>
-            <TechnologiesPopover technologies={data.technology} />
-        </ContainerProject>
-    )
+      <p>{data.text}</p>
+      <TechnologiesPopover technologies={data.technology} />
+    </ContainerProject>
+  )
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-    return {
-        paths: [],
-        fallback: 'blocking'
-    }
+  return {
+    paths: [],
+    fallback: 'blocking'
+  }
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-    const prismic = getPrismicClient({});
+  const prismic = getPrismicClient({});
 
-    // @ts-ignore
-    const { slug } = params;
+  const { slug } = params || {};
 
-    const response = await prismic.getByUID('project', String(slug), {})
+  const response = await prismic.getByUID('project', String(slug), {})
 
-    const data = {
-        uid: response.uid,
-        name: response.data.name,
-        image: asImageSrc(response.data.image),
-        text: asText(response.data.text),
-        redirect: response.data.redirect.map((element: any) => {
-            return {
-                link: element.link.url,
-                icon: asImageSrc(element.icon),
-            }
-        }),
-        technology: response.data.technology.map((element: any) => {
-            return {
-                icon: {
-                    url: asImageSrc(element.icon),
-                    field: asText(element.field)
-                }
-            }
-        }),
-    }
+  const data = {
+    uid: response.uid,
+    name: response.data.name,
+    image: asImageSrc(response.data.image),
+    text: asText(response.data.text),
+    redirect: response.data.redirect.map((element: any) => {
+      return {
+        link: element.link.url,
+        icon: asImageSrc(element.icon),
+      }
+    }),
+    technology: response.data.technology.map((element: any) => {
+      return {
+        icon: {
+          url: asImageSrc(element.icon),
+          field: asText(element.field)
+        }
+      }
+    }),
+  }
 
-    return {
-        props: {
-            data
-        },
-        revalidate: 60 * 60 * 30
-    }
+  return {
+    props: {
+      data
+    },
+    //revalidate: 60 * 60 * 30
+  }
 }
