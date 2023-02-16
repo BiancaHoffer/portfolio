719b00e9f79e1b168002e1e1bfb0d22b8ce5dcac
Bianca Hoffer <bianca.mhm1@gmail.com>
2023-02-16 18:46:02 +0000

    teste deploy 2

---
+++ src/pages/projects/[slug].tsx
@@ -0,0 +1,91 @@
+import { getPrismicClient } from "@/services/prismic"
+import { GetStaticPaths, GetStaticProps } from "next"
+import { asImageSrc, asText } from "@prismicio/helpers";
+
+import { ContainerProject } from "./styles";
+import Link from "next/link";
+import { TechnologiesPopover } from "@/components/TechnologiesPopover";
+
+interface Project {
+    uid: string;
+    name: string;
+    image: string;
+    text: string;
+    redirect: {
+        link: string;
+        icon: string;
+    }[];
+    technology: {
+        icon: {
+            url: string;
+            field: string;
+        }
+    }[];
+}
+
+interface ProjectProps {
+    data: Project
+}
+
+export default function Project({ data }: ProjectProps) {
+    return (
+        <ContainerProject>
+            <h1>{data.name}</h1>
+
+            {data.redirect.map(red => {
+                return (
+                    <Link href={red.link}>
+                        <img src={red.icon} />
+                    </Link>
+                )
+            })}
+
+            <p>{data.text}</p>
+            <TechnologiesPopover technologies={data.technology} />
+        </ContainerProject>
+    )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: [],
+        fallback: 'blocking'
+    }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const prismic = getPrismicClient({});
+
+    // @ts-ignore
+    const { slug } = params;
+
+    const response = await prismic.getByUID('project', String(slug), {})
+
+    const data = {
+        uid: response.uid,
+        name: response.data.name,
+        image: asImageSrc(response.data.image),
+        text: asText(response.data.text),
+        redirect: response.data.redirect.map((element: any) => {
+            return {
+                link: element.link.url,
+                icon: asImageSrc(element.icon),
+            }
+        }),
+        technology: response.data.technology.map((element: any) => {
+            return {
+                icon: {
+                    url: asImageSrc(element.icon),
+                    field: asText(element.field)
+                }
+            }
+        }),
+    }
+
+    return {
+        props: {
+            data
+        },
+        revalidate: 60 * 60 * 30
+    }
+}
