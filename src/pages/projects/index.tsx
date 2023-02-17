e4aaf7fcd065753650ffa559d97ab62883740e71
Bianca Hoffer <bianca.mhm1@gmail.com>
2023-02-17 11:36:06 +0000

    Teste deploy sem conexão com Prismic CMS

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -2,78 +2,26 @@ import { getPrismicClient } from "@/services/prismic";
 import { GetStaticProps } from "next";
 import Head from "next/head";
 
-import { asImageSrc } from "@prismicio/helpers";
-import { ContentProjects, ContainerProjects } from "./styles";
 import { useState } from "react";
-import Link from "next/link";
 
-interface Project {
-  uid: string;
-  name: string;
-  image: {
-    url: string;
-  }
-}
 
-interface ProjectsProps {
-  data: Project[];
-}
-
-export default function Projects({ data }: ProjectsProps) {
-  const [projects, setProjects] = useState<Project[]>(data);
-
+export default function Projects() {
   return (
     <>
       <Head>
         <title>Projetos - Bianca Hoffer</title>
       </Head>
 
-
-      <ContainerProjects>
-        <h1>Projetos</h1>
-        <p>Principais projetos até o momento</p>
-
-        <ContentProjects>
-          {projects.map((project) => {
-            return (
-              <Link href={`/projects/${project.uid}`} key={project.uid} className={project.uid}>
-                <img src={project.image.url} alt={project.name} />
-              </Link>
-            )
-          })}
-        </ContentProjects>
-      </ContainerProjects>
-
+      <h1>Projeto </h1>
     </>
   )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const prismic = getPrismicClient({});
-
-  const response = await prismic.getByType("project", {
-    orderings: {
-      field: "document.first_publication_date",
-      direction: "desc",
-    },
-    fetch: ['project.name', 'project.image'],
-    pageSize: 5,
-    page: 1,
-  })
-
-  const data = response.results.map((project) => {
-    return {
-      uid: project.uid,
-      name: project.data.name,
-      image: {
-        url: asImageSrc(project.data.image)
-      }
-    }
-  })
-
+  const prismic = getPrismicClient({})
   return {
     props: {
-      data
+      prop: "Teste"
     },
     revalidate: 60 * 60 * 24,
   }
