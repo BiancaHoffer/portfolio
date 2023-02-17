4b24fc2a3eb6a9be4c4ada1cbbe1bc9ae9435c04
Bianca Hoffer <bianca.mhm1@gmail.com>
2023-02-17 11:43:06 +0000

    Teste Prismic CMS

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -4,24 +4,48 @@ import Head from "next/head";
 
 import { useState } from "react";
 
+interface Projects {
+  name: string;
+}
 
-export default function Projects() {
+interface ProjectsProps {
+  projects: Projects[]
+}
+
+
+export default function Projects({ projects }: ProjectsProps) {
   return (
     <>
       <Head>
         <title>Projetos - Bianca Hoffer</title>
       </Head>
 
-      <h1>Projeto </h1>
+      <h1>Lista nome dos projetos</h1>
+
+      {projects.map(project => {
+        return (
+          <p>{project.name}</p>
+        )
+      })}
+
     </>
   )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const prismic = getPrismicClient({})
+  const prismic = getPrismicClient({});
+
+  const response = await prismic.getByType("project", {})
+
+  const projects = response.results.map((project) => {
+    return {
+      name: project.data.name,
+    }
+  })
+
   return {
     props: {
-      prop: "Teste"
+      projects: projects,
     },
     revalidate: 60 * 60 * 24,
   }
